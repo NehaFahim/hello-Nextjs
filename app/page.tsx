ac7de6bae755f274a941d97eea015236f57a6e30
Neha Fahim <nehafahim.098@gmail.com>
2024-10-02 12:08:56 -0700

    my-app

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+// import Image from "next/image";
+
+// export default function Home() {
+//   return (
+//   <h1>Hello World!</h1>
+
+//   );
+// };
+
+function  home(){
+  return(
+    <div>
+    <h1>H   E   L    L    O    W   O   R   L   D</h1>
+    </div>
+  )
+}
+export default home
